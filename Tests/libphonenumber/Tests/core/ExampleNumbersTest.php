c3df65fa354a0f7bb3e76e7aec73e59f24714ebc
Joshua Gigg <giggsey@gmail.com>
2013-11-10 20:58:20 +0000

    Add 'ExampleNumbersTest' and 'PhoneNumberTest'

---
+++ Tests/libphonenumber/Tests/core/ExampleNumbersTest.php
@@ -0,0 +1,297 @@
+<?php
+/**
+ *
+ *
+ * @author giggsey
+ * @created: 10/11/13 18:58
+ * @project libphonenumber-for-php
+ */
+
+namespace libphonenumber\Tests\core;
+
+use libphonenumber\Matcher;
+use libphonenumber\NumberParseException;
+use libphonenumber\PhoneNumberType;
+use libphonenumber\PhoneNumberUtil;
+use libphonenumber\RegionCode;
+use libphonenumber\ShortNumberCost;
+use libphonenumber\ShortNumberInfo;
+
+/**
+ * Verifies all of the example numbers in the metadata are valid and of the correct type. If no
+ * example number exists for a particular type, the test still passes.
+ */
+class ExampleNumbersTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var PhoneNumberUtil
+     */
+    private $phoneNumberUtil;
+    /**
+     * @var ShortNumberInfo
+     */
+    private $shortNumberInfo;
+
+    public function setUp()
+    {
+        PhoneNumberUtil::resetInstance();
+        $this->phoneNumberUtil = PhoneNumberUtil::getInstance();
+        $this->shortNumberInfo = ShortNumberInfo::getInstance($this->phoneNumberUtil);
+    }
+
+    public function regionList()
+    {
+        $returnList = array();
+
+        PhoneNumberUtil::resetInstance();
+        $phoneUtil = PhoneNumberUtil::getInstance();
+        foreach ($phoneUtil->getSupportedRegions() as $regionCode) {
+            $returnList[] = array($regionCode);
+        }
+
+        return $returnList;
+    }
+
+    /**
+     * @dataProvider regionList
+     */
+    public function testFixedLine($region)
+    {
+        $fixedLineTypes = array(PhoneNumberType::FIXED_LINE, PhoneNumberType::FIXED_LINE_OR_MOBILE);
+        $this->checkNumbersValidAndCorrectType(PhoneNumberType::FIXED_LINE, $fixedLineTypes, $region);
+    }
+
+    private function checkNumbersValidAndCorrectType($exampleNumberRequestedType, $possibleExpectedTypes, $regionCode)
+    {
+        $exampleNumber = $this->phoneNumberUtil->getExampleNumberForType($regionCode, $exampleNumberRequestedType);
+        if ($exampleNumber !== null) {
+            $this->assertTrue(
+                $this->phoneNumberUtil->isValidNumber($exampleNumber),
+                "Failed validation for {$exampleNumber}"
+            );
+
+            // We know the number is valid, now we check the type.
+            $exampleNumberType = $this->phoneNumberUtil->getNumberType($exampleNumber);
+            $this->assertContains($exampleNumberType, $possibleExpectedTypes, "Wrong type for {$exampleNumber}");
+        }
+    }
+
+    /**
+     * @dataProvider regionList
+     */
+    public function testMobile($region)
+    {
+        $mobileTypes = array(PhoneNumberType::MOBILE, PhoneNumberType::FIXED_LINE_OR_MOBILE);
+        $this->checkNumbersValidAndCorrectType(PhoneNumberType::MOBILE, $mobileTypes, $region);
+    }
+
+    /**
+     * @dataProvider regionList
+     */
+    public function testTollFree($region)
+    {
+        $tollFreeTypes = array(PhoneNumberType::TOLL_FREE);
+        $this->checkNumbersValidAndCorrectType(PhoneNumberType::TOLL_FREE, $tollFreeTypes, $region);
+    }
+
+    /**
+     * @dataProvider regionList
+     */
+    public function testPremiumRate($region)
+    {
+        $premiumRateTypes = array(PhoneNumberType::PREMIUM_RATE);
+        $this->checkNumbersValidAndCorrectType(PhoneNumberType::PREMIUM_RATE, $premiumRateTypes, $region);
+    }
+
+    /**
+     * @dataProvider regionList
+     */
+    public function testVoip($region)
+    {
+        $voipTypes = array(PhoneNumberType::VOIP);
+        $this->checkNumbersValidAndCorrectType(PhoneNumberType::VOIP, $voipTypes, $region);
+    }
+
+    /**
+     * @dataProvider regionList
+     */
+    public function testPager($region)
+    {
+        $pagerTypes = array(PhoneNumberType::PAGER);
+        $this->checkNumbersValidAndCorrectType(PhoneNumberType::PAGER, $pagerTypes, $region);
+    }
+
+    /**
+     * @dataProvider regionList
+     */
+    public function testUan($region)
+    {
+        $uanTypes = array(PhoneNumberType::UAN);
+        $this->checkNumbersValidAndCorrectType(PhoneNumberType::UAN, $uanTypes, $region);
+    }
+
+    /**
+     * @dataProvider regionList
+     */
+    public function testVoicemail($region)
+    {
+        $voicemailTypes = array(PhoneNumberType::VOICEMAIL);
+        $this->checkNumbersValidAndCorrectType(PhoneNumberType::VOICEMAIL, $voicemailTypes, $region);
+    }
+
+    /**
+     * @dataProvider regionList
+     */
+    public function testSharedCost($region)
+    {
+        $sharedCostTypes = array(PhoneNumberType::SHARED_COST);
+        $this->checkNumbersValidAndCorrectType(PhoneNumberType::SHARED_COST, $sharedCostTypes, $region);
+    }
+
+    /**
+     * @dataProvider regionList
+     */
+    public function testCanBeInternationallyDialled($regionCode)
+    {
+
+        $exampleNumber = null;
+        /** @var \libphonenumber\PhoneNumberDesc $desc */
+        $desc = $this->phoneNumberUtil->getMetadataForRegion($regionCode)->getNoInternationalDialling();
+        try {
+            if ($desc->hasExampleNumber()) {
+                $exampleNumber = $this->phoneNumberUtil->parse($desc->getExampleNumber(), $regionCode);
+            }
+        } catch (NumberParseException $e) {
+
+        }
+
+        if ($exampleNumber !== null && $this->phoneNumberUtil->canBeInternationallyDialled($exampleNumber)) {
+            $this->fail("Number {$exampleNumber} should not be internationally diallable");
+        }
+    }
+
+    public function shortNumberRegionList()
+    {
+        $returnList = array();
+
+        PhoneNumberUtil::resetInstance();
+        $phoneUtil = PhoneNumberUtil::getInstance();
+        $shortNumberInfo = ShortNumberInfo::getInstance($phoneUtil);
+        foreach ($shortNumberInfo->getSupportedRegions() as $regionCode) {
+            $returnList[] = array($regionCode);
+        }
+
+        return $returnList;
+    }
+
+    /**
+     * @dataProvider shortNumberRegionList
+     */
+    public function testEmergency($regionCode)
+    {
+        if ($regionCode == RegionCode::PG) {
+            // The only short number for Papua New Guinea is 000, which fails the test, since the
+            // national prefix is 0. This needs to be fixed.
+            return;
+        }
+
+        $desc = $this->shortNumberInfo->getMetadataForRegion($regionCode)->getEmergency();
+        if ($desc->hasExampleNumber()) {
+            $exampleNumber = $desc->getExampleNumber();
+            $possibleNumberPattern = new Matcher($desc->getPossibleNumberPattern(), $exampleNumber);
+            if (!$possibleNumberPattern->matches() || !$this->shortNumberInfo->isEmergencyNumber(
+                    $exampleNumber,
+                    $regionCode
+                )
+            ) {
+                $this->fail("Emergency example number test failed for " . $regionCode);
+            } else {
+                $emergencyNumber = $this->phoneNumberUtil->parse($exampleNumber, $regionCode);
+                if ($this->shortNumberInfo->getExpectedCost($emergencyNumber) !== ShortNumberCost::TOLL_FREE) {
+                    // TODO: Reenable this when a method is available to get the expected cost for a
+                    // particular region.
+                    // $this->fail("Emergency example number not toll free for " . $regionCode);
+                }
+            }
+        }
+    }
+
+    public function supportedGlobalNetworkCallingCodes()
+    {
+        $returnList = array();
+
+        PhoneNumberUtil::resetInstance();
+        $phoneUtil = PhoneNumberUtil::getInstance();
+        foreach ($phoneUtil->getSupportedGlobalNetworkCallingCodes() as $callingCode) {
+            $returnList[] = array($callingCode);
+        }
+
+        return $returnList;
+    }
+
+    /**
+     * @dataProvider supportedGlobalNetworkCallingCodes
+     */
+    public function testGlobalNetworkNumbers($callingCode)
+    {
+        $exampleNumber = $this->phoneNumberUtil->getExampleNumberForNonGeoEntity($callingCode);
+        $this->assertNotNull($exampleNumber, "No example phone number for calling code " . $callingCode);
+        if (!$this->phoneNumberUtil->isValidNumber($exampleNumber)) {
+            $this->fail("Failed validation for " . $exampleNumber);
+        }
+    }
+
+    /**
+     * @dataProvider regionList
+     */
+    public function getEveryRegionHasExampleNumber($regionCode)
+    {
+        $exampleNumber = $this->phoneNumberUtil->getExampleNumber($regionCode);
+        $this->assertNotNull($exampleNumber, "None found for region " . $regionCode);
+    }
+
+    /**
+     * @dataProvider shortNumberRegionList
+     */
+    public function testShortNumbersValidAndCorrectCost($regionCode)
+    {
+        if ($regionCode == RegionCode::PG) {
+            // The only short number for Papua New Guinea is 000, which fails the test, since the
+            // national prefix is 0. This needs to be fixed.
+            return;
+        }
+
+        $exampleShortNumber = $this->shortNumberInfo->getExampleShortNumber($regionCode);
+        if (!$this->shortNumberInfo->isValidShortNumber($exampleShortNumber, $regionCode)) {
+            $this->fail(
+                "Failed validation for string region_code: {$regionCode}, national_number: {$exampleShortNumber}"
+            );
+        }
+        $phoneNumber = $this->phoneNumberUtil->parse($exampleShortNumber, $regionCode);
+        if (!$this->shortNumberInfo->isValidShortNumberFromNumber($phoneNumber)) {
+            $this->fail("Failed validation for " . (string)$phoneNumber);
+        }
+
+        $costArray = array(
+            ShortNumberCost::PREMIUM_RATE,
+            ShortNumberCost::STANDARD_RATE,
+            ShortNumberCost::TOLL_FREE,
+            ShortNumberCost::UNKNOWN_COST
+        );
+
+        foreach ($costArray as $cost) {
+            $exampleShortNumber = $this->shortNumberInfo->getExampleShortNumberForCost($regionCode, $cost);
+            if ($exampleShortNumber != '') {
+                $phoneNumber = $this->phoneNumberUtil->parse($exampleShortNumber, $regionCode);
+                $this->assertEquals(
+                    $cost,
+                    $this->shortNumberInfo->getExpectedCost($phoneNumber),
+                    "Wrong cost for " . (string)$phoneNumber
+                );
+            }
+        }
+    }
+}
+
+/* EOF */
---
+++ Tests/libphonenumber/Tests/core/PhoneNumberTest.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ *
+ *
+ * @author giggsey
+ * @created: 10/11/13 20:38
+ * @project libphonenumber-for-php
+ */
+
+namespace libphonenumber\Tests\core;
+
+use libphonenumber\CountryCodeSource;
+use libphonenumber\PhoneNumber;
+
+/**
+ * Tests for the PhoneNumber object itself.
+ */
+class PhoneNumberTest extends \PHPUnit_Framework_TestCase {
+    public function testEqualSimpleNumber()
+    {
+        $numberA = new PhoneNumber();
+        $numberA->setCountryCode(1)->setNationalNumber(6502530000);
+
+        $numberB = new PhoneNumber();
+        $numberB->setCountryCode(1)->setNationalNumber(6502530000);
+
+        $this->assertEquals($numberA, $numberB);
+    }
+
+    public function testEqualWithItalianLeadingZeroSetToDefault()
+    {
+        $numberA = new PhoneNumber();
+        $numberA->setCountryCode(1)->setNationalNumber(6502530000)->setItalianLeadingZero(false);
+
+        $numberB = new PhoneNumber();
+        $numberB->setCountryCode(1)->setNationalNumber(6502530000);
+
+        // These should still be equal, since the default value for this field is false.
+        $this->assertEquals($numberA, $numberB);
+    }
+
+    public function testEqualWithCountryCodeSourceSet()
+    {
+        $numberA = new PhoneNumber();
+        $numberA->setRawInput("+1 650 253 00 00")->setCountryCode(CountryCodeSource::FROM_NUMBER_WITH_PLUS_SIGN);
+
+        $numberB = new PhoneNumber();
+        $numberB->setRawInput("+1 650 253 00 00")->setCountryCode(CountryCodeSource::FROM_NUMBER_WITH_PLUS_SIGN);
+
+        $this->assertEquals($numberA, $numberB);
+    }
+
+    public function testNonEqualWithItalianLeadingZeroSetToTrue()
+    {
+        $numberA = new PhoneNumber();
+        $numberA->setCountryCode(1)->setNationalNumber(6502530000)->setItalianLeadingZero(true);
+
+        $numberB = new PhoneNumber();
+        $numberB->setCountryCode(1)->setNationalNumber(6502530000);
+
+        $this->assertNotEquals($numberA, $numberB);
+        $this->assertFalse($numberA->equals($numberB));
+    }
+
+    public function testNonEqualWithDifferingRawInput()
+    {
+        $numberA = new PhoneNumber();
+        $numberA->setCountryCode(1)->setNationalNumber(6502530000)->setRawInput("+1 650 253 00 00")->setCountryCodeSource(CountryCodeSource::FROM_NUMBER_WITH_PLUS_SIGN);
+
+        $numberB = new PhoneNumber();
+        $numberB->setCountryCode(1)->setNationalNumber(6502530000)->setRawInput("+1-650-253-00-00")->setCountryCodeSource(CountryCodeSource::FROM_NUMBER_WITH_PLUS_SIGN);
+
+        $this->assertNotEquals($numberA, $numberB);
+        $this->assertFalse($numberA->equals($numberB));
+    }
+
+    public function testNonEqualWithPreferredDomesticCarrierCodeSetToDefault()
+    {
+        $numberA = new PhoneNumber();
+        $numberA->setCountryCode(1)->setNationalNumber(6502530000)->setPreferredDomesticCarrierCode("");
+
+        $numberB = new PhoneNumber();
+        $numberB->setCountryCode(1)->setNationalNumber(6502530000);
+
+        $this->assertNotSame($numberA, $numberB);
+        $this->assertFalse($numberA->equals($numberB));
+    }
+
+    public function testEqualWithPreferredDomesticCarrierCodeSetToDefault()
+    {
+        $numberA = new PhoneNumber();
+        $numberA->setCountryCode(1)->setNationalNumber(6502530000)->setPreferredDomesticCarrierCode("");
+
+        $numberB = new PhoneNumber();
+        $numberB->setCountryCode(1)->setNationalNumber(6502530000)->setPreferredDomesticCarrierCode("");
+
+        $this->assertEquals($numberA, $numberB);
+    }
+}
+
+/* EOF */
