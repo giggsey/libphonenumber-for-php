baad658904a7ae3b933e38087b82aea7d28b474e
Joshua Gigg <giggsey@gmail.com>
2013-12-02 18:53:57 +0000

    Add test confirming #14 is fixed
    Wasn't actually a bug, but the online demo was running an older version
    of libphonenumber-for-php that wasn't up to date.

---
+++ Tests/libphonenumber/Tests/Issues/Issue14Test.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *
+ *
+ * @author giggsey
+ * @created: 02/12/13 18:46
+ * @project libphonenumber-for-php
+ */
+namespace libphonenumber\Tests\Issues;
+
+
+use libphonenumber\PhoneNumberType;
+use libphonenumber\PhoneNumberUtil;
+
+class Issue14Test extends \PHPUnit_Framework_TestCase {
+    /**
+     * @var PhoneNumberUtil
+     */
+    private $phoneUtil;
+
+    public function setUp()
+    {
+        PhoneNumberUtil::resetInstance();
+        $this->phoneUtil = PhoneNumberUtil::getInstance();
+    }
+
+    public function testKWMobileNumber()
+    {
+        $number = "51440519";
+        $phoneNumber = $this->phoneUtil->parse($number, "KW");
+
+        $this->assertTrue($this->phoneUtil->isValidNumber($phoneNumber));
+        $this->assertEquals(PhoneNumberType::MOBILE, $this->phoneUtil->getNumberType($phoneNumber));
+    }
+}
+
+/* EOF */
