924d1d57920f9142330ea7f40fdb7e8cf9c17623
Daniel <ferkiwi@gmail.com>
2014-08-08 16:36:59 +0200

    Added PhoneNumber as serializable
    Signed-off-by: Joshua Gigg <giggsey@gmail.com>

--- src/libphonenumber/PhoneNumber.php
+++ src/libphonenumber/PhoneNumber.php
@@ -2,7 +2,7 @@
 
 namespace libphonenumber;
 
-class PhoneNumber
+class PhoneNumber implements \Serializable
 {
 
     /**
@@ -276,6 +276,47 @@ class PhoneNumber
     public function __toString()
     {
         return '+' . $this->getCountryCode() . $this->getNationalNumber();
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * String representation of object
+     * @link http://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->nationalNumber,
+            $this->countryCode,
+            $this->countryCodeSource,
+            $this->extension,
+        ));
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Constructs the object
+     * @link http://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     */
+    public function unserialize($serialized)
+    {
+        $data = unserialize($serialized);
+        // add a few extra elements in the array to ensure that we have enough keys when unserializing
+        // older data which does not include all properties.
+        //$data = array_merge($data, array_fill(0, 2, null));
+
+        list(
+            $this->nationalNumber,
+            $this->countryCode,
+            $this->countryCodeSource,
+            $this->extension,
+        ) = $data;
+
     }
 }
 
