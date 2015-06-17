f61fa94d6ea791b0888cc1f5dbfa3ad2fb9f51b8
Joshua Gigg <giggsey@gmail.com>
2015-06-17 12:05:44 +0100

    Ensure phone metadata is set before trying to use it
    Fixes #68
    
    Note: It looks like this could be a bug upstream too, will attempt to test there

---
+++ Tests/libphonenumber/Tests/Issues/Issue68Test.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace libphonenumber\Tests\Issues;
+
+
+use libphonenumber\PhoneNumberUtil;
+use libphonenumber\ShortNumberInfo;
+
+class Issue68Test extends \PHPUnit_Framework_TestCase
+{
+    public function testShortNumberInfoIsPossibleShortNumberWithRegionMissingFromCodeSet()
+    {
+        $exampleNumber = $this->getExampleNumber('NE');
+
+        $shortNumberInfo = ShortNumberInfo::getInstance();
+
+        $this->assertFalse($shortNumberInfo->isPossibleShortNumber($exampleNumber));
+    }
+
+    public function testShortNumberInfoIsPossibleShortNumberForRegionWithRegionMissingFromCodeSet()
+    {
+        $exampleNumber = $this->getExampleNumber('NE');
+
+        $shortNumberInfo = ShortNumberInfo::getInstance();
+
+        $this->assertFalse($shortNumberInfo->isPossibleShortNumberForRegion($exampleNumber, 'NE'));
+    }
+
+    private function getExampleNumber($region)
+    {
+        $phoneUtil = PhoneNumberUtil::getInstance();
+
+        $exampleNumber = $phoneUtil->getExampleNumber($region);
+
+        // Reset PhoneNumberUtil just to make sure that doesn't interfere
+        PhoneNumberUtil::resetInstance();
+
+        return $exampleNumber;
+    }
+}
