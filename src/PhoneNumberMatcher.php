373f8ad02a95e3f7cb4a20c0fabb65a8086548e3
Joshua Gigg <giggsey@gmail.com>
2018-10-04 12:34:16 +0100

    Remove unused code in PhoneNumberMatcher
    Israel phone numbers starting with * are no longer supported

--- src/PhoneNumberMatcher.php
+++ src/PhoneNumberMatcher.php
@@ -488,26 +488,6 @@ class PhoneNumberMatcher implements \Iterator
 
             $number = $this->phoneUtil->parseAndKeepRawInput($candidate, $this->preferredRegion);
 
-            // Check Israel * numbers: these are a special case in that they are four-digit numbers that
-            // our library supports, but they can only be dialled with a leading *. Since we don't
-            // actually store or detect the * in our phone number library, this means in practice we
-            // detect most four digit numbers as being valid for Israel. We are considering moving these
-            // numbers to ShortNumberInfo instead, in which case this problem would go away, but in the
-            // meantime we want to restrict the false matches so we only allow these numbers if they are
-            // preceded by a star. We enforce this for all leniency levels even though these numbers are
-            // technically accepted by isPossibleNumber and isValidNumber since we consider it to be a
-            // deficiency in those methods that they accept these numbers without the *.
-            // TODO: Remove this or make it significantly less hacky once we've decided how to
-            // handle these short codes going forward in ShortNumberInfo. We could use the formatting
-            // rules for instance, but that would be slower.
-            if ($this->phoneUtil->getRegionCodeForCountryCode($number->getCountryCode()) == "IL"
-                && mb_strlen($this->phoneUtil->getNationalSignificantNumber($number)) === 4
-                && ($offset === 0 || ($offset > 0 && mb_substr($this->text, $offset - 1, 1) != '*'))
-            ) {
-                // No match.
-                return null;
-            }
-
             if ($this->leniency->verify($number, $candidate, $this->phoneUtil)) {
                 // We used parseAndKeepRawInput to create this number, but for now we don't return the extra
                 // values parsed. TODO: stop clearing all values here and switch all users over
