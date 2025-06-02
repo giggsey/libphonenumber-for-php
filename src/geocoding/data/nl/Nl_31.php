702852d6cb6caeaebbec935f3f45a93c929e6ca0
Yurii Myronchuk <bladeroot@hiqdev.com>
2025-06-02 12:52:40 +0000

    Add special phone codes for digital systems

--- src/geocoding/data/nl/Nl_31.php
+++ src/geocoding/data/nl/Nl_31.php
@@ -16,5 +16,5 @@ namespace libphonenumber\geocoding\data\nl;
  */
 class Nl_31
 {
-    public const DATA = [3170 => 'Den Haag', 31229 => 'Hoorn', 31481 => 'Elst', 31486 => 'Schaijk'];
+    public const DATA = [3170 => 'Den Haag', 31229 => 'Hoorn', 31481 => 'Elst', 31486 => 'Schaijk', 3197 => 'Cloud communication'];
 }
